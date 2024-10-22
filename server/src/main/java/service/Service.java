c9b3ce438b82df633ead9044ae753c1a11e77a76
nexkiv <kevin@karatassos.com>
2024-10-22 13:24:03 -0600

    Created(Service) new service class

---
+++ server/src/main/java/service/Service.java
@@ -0,0 +1,16 @@
+package service;
+
+import dataaccess.DataAccess;
+
+public class Service {
+    private DataAccess dataAccess;
+
+    public Service (DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public void clear () {
+        dataAccess.clear();
+    }
+
+}
