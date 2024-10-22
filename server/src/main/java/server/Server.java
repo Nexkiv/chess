59c316d7e65a580afc82b4291074b058e56d2db1
nexkiv <kevin@karatassos.com>
2024-10-22 13:29:09 -0600

    Update(Server) added clear functionality

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,16 @@
 package server;
 
+import dataaccess.MemoryDataAccess;
 import spark.*;
+import service.Service;
 
 public class Server {
+
+    private final Service service;
+
+    public Server() {
+        service = new Service(new MemoryDataAccess());
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -10,12 +18,19 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.delete("/db", this::clearDatabase);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
         Spark.awaitInitialization();
         return Spark.port();
+    }
+
+    private Object clearDatabase(Request request, Response response) {
+        service.clear();
+        response.status(200);
+        return "";
     }
 
     public void stop() {
