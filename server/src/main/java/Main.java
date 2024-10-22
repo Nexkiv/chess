3d4edce329f38574683542de73e8a5942a902b8f
nexkiv <kevin@karatassos.com>
2024-10-21 23:10:17 -0600

    Update(main) added server functionality

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,11 @@
+import server.Server;
 import chess.*;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        Server chessServer = new Server();
+
+        System.out.printf("Server running on port %d", chessServer.run(8080));
     }
 }
+
