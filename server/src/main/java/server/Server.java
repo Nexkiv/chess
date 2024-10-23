cf533846217266a56bc20acee74b5a1c57c71127
nexkiv <kevin@karatassos.com>
2024-10-22 21:05:25 -0600

    Create(ResponseException) added foundation for exception handling

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -3,6 +3,7 @@ package server;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import dataaccess.MemoryDataAccess;
+import exception.ResponseException;
 import model.UserData;
 import spark.*;
 import service.Service;
@@ -30,6 +31,7 @@ public class Server {
         Spark.post("/game", this::createGame);
         Spark.put("/game", this::joinGame);
         Spark.get("/game", this::listGames);
+        Spark.exception(ResponseException.class, this::exceptionHandler);
 
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
@@ -39,34 +41,43 @@ public class Server {
         return Spark.port();
     }
 
-    private Object clearData(Request request, Response response) {
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+
+    private void exceptionHandler(ResponseException exception, Request request, Response response) {
+        response.status(exception.StatusCode());
+    }
+
+    private Object clearData(Request request, Response response) throws ResponseException {
         service.clear();
         response.status(200);
         return "";
     }
 
-    private Object registerUser(Request request, Response response) {
+    private Object registerUser(Request request, Response response) throws ResponseException {
         var user = new Gson().fromJson(request.body(), UserData.class);
         var auth = service.register(user);
         response.status(200);
         return auth.toJson();
     }
 
-    private Object login(Request request, Response response) {
+    private Object login(Request request, Response response) throws ResponseException {
         var user = new Gson().fromJson(request.body(), UserData.class);
         var auth = service.login(user);
         response.status(200);
         return auth.toJson();
     }
 
-    private Object logout(Request request, Response response) {
+    private Object logout(Request request, Response response) throws ResponseException {
         String authToken = request.headers("Authorization");
         service.logout(authToken);
         response.status(200);
         return "";
     }
 
-    private Object createGame(Request request, Response response) {
+    private Object createGame(Request request, Response response) throws ResponseException {
         String authToken = request.headers("Authorization");
         String gameName = request.queryParams("gameName");
         int gameID = service.create(authToken, gameName);
@@ -74,7 +85,7 @@ public class Server {
         return "{\"gameID\": " + gameID + "}";
     }
 
-    private Object joinGame(Request request, Response response) {
+    private Object joinGame(Request request, Response response) throws ResponseException {
         String authToken = request.headers("Authorization");
         JsonObject jsonObject = new Gson().fromJson(request.body(), JsonObject.class);
         String playerColor = jsonObject.get("playerColor").getAsString();
@@ -84,16 +95,10 @@ public class Server {
         return "";
     }
 
-    private Object listGames(Request request, Response response) {
+    private Object listGames(Request request, Response response) throws ResponseException {
         String authToken = request.headers("Authorization");
         String games = "{\"games\": " + service.list(authToken) + "}";
         response.status(200);
         return games;
-    }
-
-
-    public void stop() {
-        Spark.stop();
-        Spark.awaitStop();
     }
 }
