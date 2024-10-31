0dc20d51c23d95983a155b74681fac3464d46ae7
nexkiv <kevin@karatassos.com>
2024-10-30 22:52:03 -0600

    Created(DataAccessTests)

---
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -0,0 +1,23 @@
+/*
+Database Unit Tests
+
+    The pass off tests do not examine your game board.
+    That means it is critical that you write tests that fully test everything you are persisting to the database.
+    This includes tests that store an initial board, add players, make moves, and update the game state.
+
+    As part of your unit test deliverable you need to meet the following requirements.
+
+    Write a positive and a negative JUNIT test case for each public method on your DAO classes,
+    except for Clear methods which only need a positive test case.
+    A positive test case is one for which the action happens successfully
+    (e.g., creating a new user in the database).
+    A negative test case is one for which the operation fails
+    (e.g., creating a User that has the same username as an existing user).
+    Ensure that all of your unit tests work,
+    including the new DAO tests and the Service tests you wrote in the previous assignment.
+ */
+
+package dataaccess;
+
+public class DataAccessTests {
+}
