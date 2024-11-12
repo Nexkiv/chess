184a2fbf215987db63bf04059133f30effa9793d
nexkiv <kevin@karatassos.com>
2024-11-12 12:30:35 -0700

    Updated(clientInterface)

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,30 +1,5 @@
 package ui;
 
-import chess.ChessGame;
-import com.google.gson.Gson;
-import exception.ResponseException;
-import server.ServerFacade;
-
-public class ChessClient {
-    private String playerName = null;
-    private final ServerFacade server;
-    private final String serverUrl;
-    private State state = State.SIGNEDOUT;
-
-    public ChessClient(String serverUrl) {
-        server = new ServerFacade(serverUrl);
-        this.serverUrl = serverUrl;
-    }
-
-    public String help() {
-        if (state == State.SIGNEDOUT) {
-            return ("""
-                        register <USERNAME> <PASSWORD> <EMAIL> - to create an account
-                        login <USERNAME> <PASSWORD> - to play chess
-                        quit - closes the chess UI
-                        help - display possible commands with explanations
-                    """);
-        }
-        return null;
-    }
+public interface ChessClient {
+    String help();
 }
---
+++ client/src/main/java/ui/LoggedOutClient.java
@@ -0,0 +1,24 @@
+package ui;
+
+import server.ServerFacade;
+
+public class LoggedOutClient implements ChessClient {
+    private String playerName = null;
+    private final ServerFacade server;
+    private final String serverUrl;
+    private State state = State.SIGNEDOUT;
+
+    public LoggedOutClient(String serverUrl) {
+        server = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
+    }
+
+    public String help() {
+        return ("""
+                    register <USERNAME> <PASSWORD> <EMAIL> - to create an account
+                    login <USERNAME> <PASSWORD> - to play chess
+                    quit - closes the chess UI
+                    help - display possible commands with explanations
+                """);
+    }
+}
