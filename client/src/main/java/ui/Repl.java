21a22021992eded7409dbd3bedc8b63700169e33
nexkiv <kevin@karatassos.com>
2024-11-12 08:57:30 -0700

    Created(serverFacade) preping

---
+++ client/src/main/java/ui/Repl.java
@@ -0,0 +1,17 @@
+package ui;
+
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
+public class Repl {
+    private final ChessClient client;
+
+    public Repl(String serverUrl) {
+        client = new ChessClient(serverUrl);
+    }
+
+    public void run() {
+        System.out.println(BLACK_KING + " Welcome to Chess! Sign-in to Start! " + BLACK_KING);
+    }
+}
