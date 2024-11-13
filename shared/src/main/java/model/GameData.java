fc1c9f1dcc1dbb93d7ab1d89f671213da53e1635
nexkiv <kevin@karatassos.com>
2024-11-12 22:28:27 -0700

    Update (gameData) made gameID an Integer

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -1,11 +1,10 @@
 package model;
 
 import chess.ChessGame;
-import com.google.gson.Gson;
 
 import java.util.Objects;
 
-public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+public record GameData(Integer gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
 
     @Override
     public boolean equals(Object o) {
