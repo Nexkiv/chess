c03c5920a9d39cf6f063d8cd1db97ada93f06431
nexkiv <kevin@karatassos.com>
2024-11-26 18:16:52 -0500

    Update (MakeMoveCommand) fixed move construction

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -4,11 +4,11 @@ package websocket.commands;
 import chess.ChessMove;
 
 public class MakeMoveCommand extends UserGameCommand {
-    private ChessMove move;
+    private final ChessMove move;
 
-    public MakeMoveCommand(CommandType commandType, String authToken, Integer gameID) {
+    public MakeMoveCommand(CommandType commandType, String authToken, Integer gameID, ChessMove move) {
         super(commandType, authToken, gameID);
-        move = null;
+        this.move = move;
     }
 
     public ChessMove getMove() {
